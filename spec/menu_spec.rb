ee7b6eb20a3309bdc17967f453d856719974ee64
Sean Redmond <github-smr@sneakemail.com>
2012-12-28 18:04:55 -0500

    Menu tests, (and helper)

---
+++ spec/menu_spec.rb
@@ -0,0 +1,129 @@
+require "spec_helper"
+
+describe Hungry::Menu do
+  describe "Basic initialization" do
+    before :each do
+      @menu = Hungry::Menu.new(MENU_31729, double(Hungry::Client))
+    end
+
+    it "has an id" do
+      @menu.id.should eq 31729
+    end
+
+    it "has a name" do
+      @menu.name.should be_nil
+    end
+
+    it "has a sponsor" do
+      @menu.sponsor.should eq "Le Versailles"
+    end
+
+    it "has a venue" do
+      @menu.venue.should be_nil
+    end
+
+    it "has an event" do
+      @menu.event.should be_nil
+    end
+
+    it "has a place" do
+      @menu.place.should be_nil
+    end
+
+    it "has an occasion" do
+      @menu.occasion.should be_nil
+    end
+
+    it "has a physical_description" do
+      @menu.physical_description.should eq "35.5x29.5cm"
+    end
+
+    it "has notes" do
+      @menu.notes.should eq "  "
+    end
+
+    it "has a call_number" do
+      @menu.call_number.should eq "1920-0071_wotm"
+    end
+
+    it "has a keywords" do
+      @menu.keywords.should be_nil
+    end
+
+    it "has a language" do
+      @menu.language.should be_nil
+    end
+
+    it "has a year" do
+      @menu.year.should eq 1920
+    end
+
+    it "has a month" do
+      @menu.month.should eq 6
+    end
+
+    it "has a day" do
+      @menu.day.should eq 14
+    end
+
+    it "has a status" do
+      @menu.status.should eq "complete"
+    end
+
+    it "has a currency" do
+      @menu.currency.should eq "Francs"
+    end
+
+    it "has a currency_symbol" do
+      @menu.currency_symbol.should eq "FF"
+    end
+
+    it "has a location" do
+      @menu.location.should eq "Le Versailles"
+    end
+
+    it "has a location_type" do
+      @menu.location_type.should be_nil
+    end
+
+    it "has a page_count" do
+      @menu.page_count.should eq 2
+    end
+
+    it "has a dish_count" do
+      @menu.dish_count.should eq 168
+    end
+
+    it "has a thumbnail_src" do
+      @menu.thumbnail_src.should eq "http://images.nypl.org/index.php?id=4029915&t=r"
+    end
+
+    it "has a thumbnail_src_jp2" do
+      @menu.thumbnail_src_jp2.should eq "http://j2k.repo.nypl.org/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=urn:uuid:bf8b24be-cb02-4876-e040-e00a1806540b&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=108,108&svc.region=627,240,3431,3431&svc.rotate=0"
+    end
+
+    it "has a large_src" do
+      @menu.large_src.should eq "http://images.nypl.org/index.php?id=4029915&t=w"
+    end
+
+    it "has a large_src_jp2" do
+      @menu.large_src_jp2.should eq "http://j2k.repo.nypl.org/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=urn:uuid:bf8b24be-cb02-4876-e040-e00a1806540b&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=1800,0&svc.rotate=0"
+    end
+
+    it "has a first_page_aspect" do
+      @menu.first_page_aspect.should eq "portrait"
+    end
+
+    it "has a first_page_full_width" do
+      @menu.first_page_full_width.should eq 3911
+    end
+
+    it "has a first_page_full_height" do
+      @menu.first_page_full_height.should eq 4686
+    end
+
+    it "has links" do
+      @menu.links.should be_an_instance_of Array
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require "hungry"
+
+MENU_31729 = menu_json = {"id"=>31729, "name"=>nil, "sponsor"=>"Le Versailles", 
+  "venue"=>nil, "event"=>nil, "place"=>nil, "occasion"=>nil, 
+  "physical_description"=>"35.5x29.5cm", "notes"=>"  ", 
+  "call_number"=>"1920-0071_wotm", "keywords"=>nil, "language"=>nil, 
+  "year"=>1920, "month"=>6, "day"=>14, "status"=>"complete", 
+  "currency"=>"Francs", "currency_symbol"=>"FF", 
+  "location"=>"Le Versailles", "location_type"=>nil, "page_count"=>2, 
+  "dish_count"=>168, 
+  "thumbnail_src"=>"http://images.nypl.org/index.php?id=4029915&t=r", 
+  "thumbnail_src_jp2"=>"http://j2k.repo.nypl.org/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=urn:uuid:bf8b24be-cb02-4876-e040-e00a1806540b&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=108,108&svc.region=627,240,3431,3431&svc.rotate=0", 
+  "large_src"=>"http://images.nypl.org/index.php?id=4029915&t=w", 
+  "large_src_jp2"=>"http://j2k.repo.nypl.org/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id=urn:uuid:bf8b24be-cb02-4876-e040-e00a1806540b&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.scale=1800,0&svc.rotate=0", 
+  "first_page_aspect"=>"portrait", 
+  "first_page_full_width"=>3911, 
+  "first_page_full_height"=>4686, 
+  "links"=>[
+    {"href"=>"http://menus.nypl.org/api/menus", "rel"=>"index"}, 
+    {"href"=>"http://menus.nypl.org/api/menus/31729/pages", "rel"=>"pages"}, 
+    {"href"=>"http://menus.nypl.org/api/menus/31729/dishes", "rel"=>"dishes"} 
+  ]
+}
