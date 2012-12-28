53499ac75c380a3dd5c48aac358e8cff0f85cdc9
Sean Redmond <github-smr@sneakemail.com>
2012-12-28 18:01:16 -0500

    Add dynamic properties, client
    Add client to constructor parameters and save as instance variable
    
    Since this is a wrapper for JSON data that's been parsed into a hash, use method_missing to lookup keys in the hash and use them like properties of the class.

--- lib/hungry/menu.rb
+++ lib/hungry/menu.rb
@@ -1,7 +1,16 @@
 module Hungry
   class Menu
-    def initialize(data)
+    def initialize(data, client)
       @data = data
+      @client = client
+    end
+
+    def method_missing name, *args
+      name = name.to_s
+      if args.empty? && @data.keys.include?(name)
+        return @data[name]
+      end
+      raise NoMethodError, "undefined method `#{name}' for #{self}"
     end
   end
 end
